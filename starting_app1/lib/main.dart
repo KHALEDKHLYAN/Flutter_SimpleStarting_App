120ad299042d486b8e03d2bcb8eda786828aa088
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-11-09 23:43:56 +0300

    personal

--- starting_app1/lib/main.dart
+++ starting_app1/lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,13 +16,46 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text('PortFolio',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+          title: const Text(
+            'PortFolio',
+            style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+          ),
           backgroundColor: Colors.blueAccent,
           actions: [
-            IconButton(icon: Icon(Icons.person_2_rounded), onPressed: (){} )
+            IconButton(
+                icon: const Icon(Icons.person_2_rounded), onPressed: () {})
           ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                accountEmail: Text('Khaledkhlyan@gmail.com'),
+                accountName: Text('KHALED_4Cus'),
+                currentAccountPicture: CircleAvatar(
+                  foregroundImage: AssetImage('assets/bleach.jpg'),
+                ),
+              ),
+              customBuildListTile(Icons.home, 'Home Page 1', () { 
+                Navigator.pop(context);
+              }),
+              customBuildListTile(Icons.train, 'Train Page 2', () { 
+                Navigator.pop(context);
+              }),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+
+ListTile customBuildListTile(IconData icon, String text, VoidCallback onTap) {
+  return ListTile(
+    leading: Icon(icon),
+    title: Text(text),
+    onTap: onTap,
+  );
 }
